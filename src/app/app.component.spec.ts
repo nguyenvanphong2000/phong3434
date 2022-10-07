6088f6832079b9856e1ae953718eb98a90c75c93
nguyenvanphong2000 <110529560+nguyenvanphong2000@users.noreply.github.com>
2022-10-07 15:18:28 +0700

    firtcommit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'untitled2'`, () => {
+  it(`should have as title 'Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('untitled2');
+    expect(app.title).toEqual('Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('untitled2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular app is running!');
   });
 });
